e2e5c53eb6d10800fd2b57656a98ed0a285a0ef6
Soyeon Ahn <soyeonahn@Soyeons-MacBook.local>
2023-11-30 14:18:54 -0800

    merge into one model

--- final-project/build/controllers/userController.js
+++ final-project/build/controllers/userController.js
@@ -1,27 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.loginUser = exports.createUser = exports.getUser = exports.getAllUsers = void 0;
+exports.deleteUser = exports.updateUser = exports.getUser = exports.getAllUsers = exports.loginUser = exports.createUser = void 0;
 const user_1 = require("../models/user");
 const auth_1 = require("../services/auth");
-const getAllUsers = async (req, res, next) => {
-    let users = await user_1.User.findAll();
-    res.status(200).json(users);
-};
-exports.getAllUsers = getAllUsers;
-const getUser = async (req, res, next) => {
-    let userId = req.params.id;
-    let user = await user_1.User.findByPk(userId);
-    // let user: User | null = await verifyToken(req);
-    if (user) {
-        res.status(200).json(user);
-    }
-    else {
-        res.status(404).json({});
-    }
-};
-exports.getUser = getUser;
 const createUser = async (req, res, next) => {
     let newUser = req.body;
+    console.log(newUser);
     if (newUser.username && newUser.password) {
         let hashedPassword = await (0, auth_1.hashPassword)(newUser.password);
         newUser.password = hashedPassword;
@@ -58,3 +42,77 @@ const loginUser = async (req, res, next) => {
     }
 };
 exports.loginUser = loginUser;
+const getAllUsers = async (req, res, next) => {
+    let users = await user_1.User.findAll();
+    res.status(200).json(users);
+};
+exports.getAllUsers = getAllUsers;
+const getUser = async (req, res, next) => {
+    let user = await (0, auth_1.verifyToken)(req);
+    if (!user) {
+        return res.status(403).send(); //403 forbidden if user is not logged in 
+    }
+    let userId = req.params.id;
+    let userFound = await user_1.User.findByPk(userId);
+    // console.log(user)
+    // console.log(userId)
+    // let user: User | null = await verifyToken(req);
+    if (userFound && userFound.userId == user.userId) {
+        res.status(200).json(user);
+    }
+    else {
+        res.status(404).json({});
+    }
+};
+exports.getUser = getUser;
+const updateUser = async (req, res, next) => {
+    let user = await (0, auth_1.verifyToken)(req);
+    // console.log(user)
+    if (!user) {
+        return res.status(403).send(); //403 forbidden if user is not logged in 
+    }
+    let userId = req.params.id;
+    let newProfile = req.body;
+    console.log(userId);
+    console.log(newProfile);
+    let userFound = await user_1.User.findByPk(userId);
+    if (userFound && userFound.userId == newProfile.userId
+        && newProfile.firstName) {
+        if (userFound.userId == user.userId) {
+            await user_1.User.update(newProfile, {
+                where: { userId: userId }
+            });
+            res.status(200).json();
+        }
+        else {
+            res.status(403).send();
+        }
+    }
+    else {
+        res.status(400).json();
+    }
+};
+exports.updateUser = updateUser;
+const deleteUser = async (req, res, next) => {
+    let user = await (0, auth_1.verifyToken)(req);
+    if (!user) {
+        return res.status(403).send(); //403 forbidden if user is not logged in 
+    }
+    let userId = req.params.id;
+    let userFound = await user_1.User.findByPk(userId);
+    if (userFound) {
+        if (userFound.userId == user.userId) {
+            await user_1.User.destroy({
+                where: { userId: userId }
+            });
+            res.status(200).json();
+        }
+        else {
+            res.status(403).send();
+        }
+    }
+    else {
+        res.status(404).json();
+    }
+};
+exports.deleteUser = deleteUser;
--- final-project/src/routes/userRoutes.ts
+++ final-project/src/routes/userRoutes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
-import { createUser, loginUser, getUser, getAllUsers } from '../controllers/userController';
+import { createUser, loginUser, getUser, getAllUsers, updateUser, deleteUser } from '../controllers/userController';
 
 const router = Router();
 
-router.get('/', getAllUsers);
+
 router.post('/', createUser);
 router.post('/login', loginUser);
+router.get('/', getAllUsers);
 router.get('/:id',getUser);
+router.put('/:id',updateUser);
+router.delete('/:id', deleteUser);
 
 export default router;
