7528c60a6d76c1f31043ddd28418cd6b7ba6d266
allyshia lucas <allyshia18@gmail.com>
2023-12-10 00:40:58 -0500

    added product controller and routes

---
+++ final-project/src/routes/productRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { getAllProducts, getProduct, deleteProduct, updateProduct } from './controllers/productController';
+
+const router = Router();
+
+router.get('/', getAllProducts);
+router.get('/:productId', getProduct);
+router.put('/:productId', updateProduct);
+router.delete('/:productId', deleteProduct);
+
+export default router;
