340565f028416b232b9f1c15220f0bbb5a717761
Jude Prashanth <goodonone@outlook.com>
2024-02-01 12:56:41 -0800

    Project Complete

---
+++ celebrityHealthProject-Backend/src/app.ts
@@ -0,0 +1,36 @@
+import express, { NextFunction, Request, Response } from 'express'
+import morgan from 'morgan';
+import { db } from './models';
+import userRoutes from './routes/userRoutes';
+import productRoutes from './routes/productRoutes';
+import paymentRoutes from './routes/paymentRoutes';
+import cartRoutes from './routes/cartRoutes';
+
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// routes
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/cart', cartRoutes);
+app.use('/api/payment', paymentRoutes);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).end();
+});
+
+// Syncing our database
+db.sync({ alter: true }).then(() => {
+//db.sync().then(() => {
+    console.info("connected to the database!")
+});
+
+// app.listen(3001);
+app.listen(3000);
---
+++ celebrityHealthProject-Backend/src/models/index.ts
@@ -0,0 +1,72 @@
+import { Sequelize } from "sequelize";
+import { UserFactory } from "./user";
+import { ProductFactory } from "./product";
+import { CartFactory, AssociateCartUser } from "./cart";
+import { CartProductFactory, AssociateCartProduct } from "./cart-product";
+import { PaymentFactory, AssociateUserPayment } from "./payment";
+import { productList } from './productList';
+import { Product } from "./product"; 
+
+
+const dbName = 'FitnessAPI2';
+const username = 'root';
+const password = 'Triumph23';
+
+const sequelize = new Sequelize(dbName, username, password, {
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql'
+});
+
+UserFactory(sequelize);
+ProductFactory(sequelize);
+CartFactory(sequelize);
+PaymentFactory(sequelize);
+CartProductFactory(sequelize);
+
+AssociateCartProduct();
+AssociateCartUser();
+AssociateUserPayment();
+
+// Function to sync database and seed
+async function initializeDatabase() {
+    try {
+        // Sync all models
+        // await sequelize.sync({ force: true });
+        await sequelize.sync();
+        console.log("Database synchronized");
+
+        // Seed the database
+        // await Product.bulkCreate(productList);
+        // console.log("Database seeded!");
+        
+        // Optionally seed the database
+        // Check if the products table is empty before seeding
+        const productCount = await Product.count();
+        console.log("---------------------------",productCount)
+        // if (productCount === 0) {
+        if (productCount <= 10) {
+            await Product.bulkCreate(productList);
+            console.log("Database seeded!");
+        } else {
+            console.log("Database already has data, skipping seeding.");
+        }
+    } catch (error) {
+        console.error("Error initializing database:", error);
+    }
+}
+
+// Initialize the database
+initializeDatabase();
+
+// Function to seed the database
+// async function seedDatabase() {
+//     await sequelize.sync({ force: true }); // Be cautious with `force: true`
+//     await Product.bulkCreate(productList);
+// }
+
+// // Call the seed function
+// seedDatabase().then(() => console.log("Database seeded!")).catch(error => console.error("Error seeding database:", error));
+
+
+export const db = sequelize;
